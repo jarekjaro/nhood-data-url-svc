e36258f82e5f73f567b1c28a04a6d2c6ab55447f
krzysztofreczek <reczek.krzysztof@gmail.com>
2018-09-11 19:59:50 +0200

    NHLS_F1: initial commit

---
+++ src/main/java/com/h8/nh/nhoodlocationsvc/NhoodLocationSvcApplication.java
@@ -0,0 +1,12 @@
+package com.h8.nh.nhoodlocationsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhoodLocationSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhoodLocationSvcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/h8/nh/nhoodlocationsvc/NhoodLocationSvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.h8.nh.nhoodlocationsvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NhoodLocationSvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
