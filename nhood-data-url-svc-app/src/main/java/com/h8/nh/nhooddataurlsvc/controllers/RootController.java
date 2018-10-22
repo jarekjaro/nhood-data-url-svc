55863f0b3dc69f95af1ea42a1d12e8541fd99308
krzysztofreczek <reczek.krzysztof@gmail.com>
2018-10-22 22:19:01 +0200

    Add basic root controller

---
+++ nhood-data-url-svc-app/src/main/java/com/h8/nh/nhooddataurlsvc/controllers/RootController.java
@@ -0,0 +1,25 @@
+package com.h8.nh.nhooddataurlsvc.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${spring.application.version}")
+    private String applicationVersion;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<String> root() {
+        var message = String.format("%s:%s", applicationName, applicationVersion);
+        return ResponseEntity.ok(message);
+    }
+}
